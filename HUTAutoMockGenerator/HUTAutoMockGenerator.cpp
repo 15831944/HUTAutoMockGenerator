48e6a9331c712cc83ba79811ef41a8efa9c4ea00
Patan Amrulla Khan <pamrulla@gmail.com>
2019-10-09 14:16:51 +0530

    Updated to create basic mock class

--- HUTAutoMockGenerator/HUTAutoMockGenerator.cpp
+++ HUTAutoMockGenerator/HUTAutoMockGenerator.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <clang-c/Index.h>
 
 std::ostream& operator<<(std::ostream& stream, const CXString& str)
@@ -6,6 +8,75 @@ std::ostream& operator<<(std::ostream& stream, const CXString& str)
 	stream << clang_getCString(str);
 	clang_disposeString(str);
 	return stream;
+}
+
+std::string ToString(const CXString& s)
+{
+	std::string result = clang_getCString(s);
+	clang_disposeString(s);
+	return result;
+}
+
+void parseUsrString(const std::string& usrString, bool* isConst) {
+	size_t bangLocation = usrString.find("#&");
+	if (bangLocation == std::string::npos || bangLocation == usrString.length() - 1) {
+		*isConst = false;
+		return;
+	}
+	bangLocation += 2;
+	int x = usrString[bangLocation];
+
+	*isConst = x & 0x1;
+}
+
+void print_function_prototype(CXCursor cursor)
+{
+	auto type = clang_getCursorType(cursor);
+	
+	auto function_name = ToString(clang_getCursorSpelling(cursor));
+	auto return_type = ToString(clang_getTypeSpelling(clang_getResultType(type)));
+	CXString usr = clang_getCursorUSR(cursor);
+	std::string usr_string = ToString(usr);
+	bool isConst = false;
+	parseUsrString(usr_string, &isConst);
+	std::string MockMacro = "MOCK_METHOD";
+	if (isConst)
+	{
+		MockMacro = "MOCK_CONST_METHOD";
+	}
+	int num_args = clang_Cursor_getNumArguments(cursor);
+	std::cout << "\t" << MockMacro << num_args << "("
+		<< function_name << ", " << return_type << "(";
+	for (int i = 0; i < num_args; ++i)
+	{
+		auto arg_cursor = clang_Cursor_getArgument(cursor, i);
+		auto arg_name = ToString(clang_getCursorSpelling(arg_cursor));
+		if (arg_name.empty())
+		{
+			arg_name = "no name!";
+		}
+
+		auto arg_data_type = ToString(clang_getTypeSpelling(clang_getArgType(type, i)));
+		std::cout << arg_data_type << " " << arg_name;
+		if (i != num_args - 1)
+		{
+			std::cout << ", ";
+		}
+	}
+
+	std::cout << "));" << std::endl;
+}
+
+CXChildVisitResult FindMethods(CXCursor c, CXCursor p, CXClientData cd)
+{
+	CXCursorKind kind = clang_getCursorKind(c);
+	if (kind == CXCursorKind::CXCursor_CXXMethod ||
+		kind == CXCursorKind::CXCursor_FunctionDecl)
+	{
+		
+		print_function_prototype(c);
+	}
+	return CXChildVisit_Continue;
 }
 
 int main()
@@ -20,7 +91,7 @@ int main()
 		0,
 		nullptr,
 		0,
-		0,
+		CXTranslationUnit_SkipFunctionBodies,
 		&unit
 	);
 
@@ -34,8 +105,32 @@ int main()
 	clang_visitChildren(cursor,
 		[](CXCursor c, CXCursor p, CXClientData cd)
 		{
-			std::cout << "Cursor '" << clang_getCursorSpelling(c) << "' of kind '"
-				<< clang_getCursorKindSpelling(clang_getCursorKind(c)) << "'\n";
+			if (clang_getCursorKind(c) == CXCursorKind::CXCursor_ClassDecl)
+			{
+				std::string name = ToString(clang_getCursorSpelling(c));
+				if (name == "Test")
+				{
+					std::string newName = "Mock" + name;
+					std::string statPointer = "p" + newName;
+					std::cout << "class Mock" << name << std::endl;
+					std::cout << "{" << std::endl;
+					std::cout << "public:" << std::endl;
+					std::cout << "\t" << "static " << newName << "* " << statPointer << ";" << std::endl;
+					std::cout << "\t" << newName << "()" << std::endl;
+					std::cout << "\t" << "{" << std::endl;
+					std::cout << "\t\t" << statPointer << " = this;" << std::endl;
+					std::cout << "\t" << "}" << std::endl;
+					std::cout << std::endl;
+					std::cout << "\t" << "~" << newName << "()" << std::endl;
+					std::cout << "\t" << "{" << std::endl;
+					std::cout << "\t\t" << statPointer << " = nullptr;" << std::endl;
+					std::cout << "\t" << "}" << std::endl;
+					std::cout << std::endl;
+
+					clang_visitChildren(c, FindMethods, nullptr);
+					std::cout << "};" << std::endl;
+				}
+			}
 			return CXChildVisit_Recurse;
 		},
 		nullptr);
--- HUTAutoMockGenerator/TestData/test.hpp
+++ HUTAutoMockGenerator/TestData/test.hpp
@@ -1,18 +1,41 @@
 /* this is comment */
+namespace np
+{
+	class Test
+	{
+	private:
+		int a;
+		char b;
 
-class Test
+		void* privateMethod(const int& x, int y) const {
+			int xz = 0;
+		}
+	public:
+		Test() {}
+		virtual ~Test() {}
+
+		virtual int __stdcall publicVirtualMehtod(int a, int b, int c) = 0;
+
+		static float statFloat;
+		static void staticMethod() {}
+	};
+}
+
+class Test1
 {
 private:
-	int a;
-	char b;
+	int a1;
+	char b1;
 
-	void privateMethod(int x, int y) {}
+	void privateMethod1(int x1, int y1) {
+		int xz1 = 0;
+	}
 public:
-	Test() {}
-	virtual ~Test() {}
+	Test1() {}
+	virtual ~Test1() {}
 
-	virtual int publicVirtualMehtod(int a, int b, int c) = 0;
+	virtual int publicVirtualMehtod1(int a1, int b1, int c1) = 0;
 
-	static float statFloat;
-	static void staticMethod() {}
+	static float statFloat1;
+	static void staticMethod1() {}
 };
